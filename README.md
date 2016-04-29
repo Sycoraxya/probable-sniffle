36a6c625cbf6dad8ade75e1a6e2e48edf7e0625a
sycoraxya <stefanwv@live.nl>
2016-04-29 14:24:38 +0200

    added functionality to the script and cleaned up the older code. Also reformatted the HTML and added styling

--- README.md
+++ README.md
@@ -6,25 +6,51 @@ Testimonials carousel
 ```html
 <div class="testimonial-wrapper">
     <section class="testimonial">
-        <span class="info">
-            <span class="image"><img src="img/oscar.jpg"></span>
-            <span class="name">Oscar George Bluth</span> 
-            <span class="company">Probike tweewielers</span>
-        </span>
         <span class="quote">
-            there's always money in the banana stand!
+            There's always money in the banana stand!
+        </span>
+        <span class="info">
+            <span class="image" style="background-image: url(img/oscar.jpg)"></span>
+            <span class="inf-wrap">
+                <span class="name">Oscar George Bluth</span> 
+                <span class="company">Probike tweewielers</span>
+            </span>
         </span>
     </section>
     <section class="testimonial">
-        <span class="info">
-            <span class="image"><img src="img/michael.png"></span>
-            <span class="name">Michael bluth</span> 
-            <span class="company">The bluth company</span>
-        </span>
         <span class="quote">
-            You buy yourself a tape recorder, you just record yourself for a 
-            whole day. I think you’re going to be surprised at some of your phrasing.
+            You buy yourself a tape recorder, you just record yourself for a whole day. I think you’re going to be surprised at some of your phrasing.
+        </span>
+        <span class="info">
+            <span class="image" style="background-image: url(img/michael.jpg)"></span>
+            <span class="inf-wrap">
+                <span class="name">Michael bluth</span> 
+                <span class="company">The bluth company</span>
+            </span>
         </span>
     </section>
+    <section class="testimonial">
+        <span class="quote">
+            Marry me!
+        </span>
+        <span class="info">
+            <span class="image" style="background-image: url(img/maeby.jpg)"></span>
+            <span class="inf-wrap">
+                <span class="name">Maeby Fünke</span> 
+                <span class="company">Tantamount Studios</span>
+            </span>
+        </span>
+    </section>
+    <span class="navigation"></span>
 </div>
+```
+
+## USAGE 
+
+Drop the HTML somewhere in your document, add the css to your stylesheet and call the script using:
+
+```js
+(function () {
+    testimonials.init($('.testimonial-wrapper'));
+})();
 ```
--- js/app.js
+++ js/app.js
@@ -1,72 +1,74 @@
 /* 
-    Created on : 26-Apr-2016, 14:24:51
-    Author     : Stefan Verweij
-*/
+ Created on : 26-Apr-2016, 14:24:51
+ Author     : Stefan Verweij
+ */
+
+/**
+ * TODO: add create function and call using a function constructor. See
+ * https://github.com/Sycoraxya/effective-dollop
+ * 
+ * var testimonialsTest = Object.create(Testimonials);
+ * testimonialsTest.init($('.testimonial-wrapper'), 'testimonialsTest')
+ */
 
 var testimonials = {
     animationType: 'fade', // fade or slide
     speed: 5000, // time each testimonial is visible
     testimonialElements: [], // Object of all <section> elements in the wrapper. {element, text, image, name, company}
-    wrapperElement: '', 
+    wrapperElement: '',
     $wrapperElement: [],
     currentSlide: 1,
-   init: function (wrapperElement) {
+    init: function (wrapperElement) {
         this.$wrapperElement = $(wrapperElement);
         this.wrapperElement = wrapperElement[0].className;
         this.getAllElements();
         console.log(this.testimonialElements);
         this.addNav();
-        
+        this.setActive(this.currentSlide);
+        this.bindClicks();
     },
     getAllElements: function () {
         var elements = $('.' + this.wrapperElement + ' section');
-        
+
         // loop through all elements, create seperate objects from them and store them all in this.testimonialElements
         for (i = 0; i < elements.length; i++) {
             var testimonialElement = elements[i],
                 $testimonialElement = $(testimonialElement);
-            
-            testimonials.testimonialElements.push({ 
+
+            $testimonialElement.attr('data-index', (i + 1));
+
+            testimonials.testimonialElements.push({
                 element: elements[i],
-                info: this.getInfo($testimonialElement)
             });
-            
-            $testimonialElement.attr('data-index', (i +1)).find('.info').detach();
-            this.addPerson(i);
-            /**
-             * TODO: add data-index to testimonials
-             */
         }
-    },
-    getInfo: function (element) {
-        return element.find('.info');
-    },
-    getImage: function (element) {
-        return element.find('.image').css('backgroundImage').replace('url("', '').replace('")', '');
-    },
-    getName: function (element) {
-        return element.find('.name').text().trim();
-    },
-    getCompany: function (element) {
-        return element.find('.company').text().trim();
-    },
-    addPerson: function (index) {
-        this.$wrapperElement.find('.persons').append((this.testimonialElements[index].info).attr('data-index', (index +1)));
     },
     addNav: function () {
         console.log('Adding ' + this.testimonialElements.length + ' navigation spheres...');
         for (i = 1; i < (this.testimonialElements.length + 1); i++) {
-            $('.testimonial-wrapper .navigation').append('<span class="nav-sphere" data-index="' + i +'"></span>');
-        }    
+            $('.' + this.wrapperElement + ' .navigation').append('<span class="nav-sphere" data-index="' + i + '"></span>');
+        }
+    },
+    setActive: function (slide) {
+        if ($('.' + this.wrapperElement + ' .active').length) {
+            $('.' + this.wrapperElement + ' .active').removeClass('active');
+        }
+        $('section[data-index="' + slide + '"').addClass('active');
+        $('.navigation span[data-index="' + slide + '"').addClass('active');
+    },
+    bindClicks: function () {
+        this.$wrapperElement.find('.navigation span').bind("click", function (e) {
+            testimonials.animateTo(e.target);
+        });
     },
     next: function () {
-        
+
     },
     prev: function () {
-        
+
     },
-    animateTo: function () {
-        
+    animateTo: function (e) {
+        var index = $(e).attr('data-index');
+        this.setActive(index);
     }
 };
 
